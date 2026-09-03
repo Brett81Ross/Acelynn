816729530d695e734972b807b85920f0a92ebb1e
Brett  Ross <rossbrett05@gmail.com>
2026-09-03 11:06:21 -0500

    meta: advance Acelynn Pro staging metadata to v1.3.0

--- js/meta.js
+++ js/meta.js
@@ -1,14 +1,16 @@
 export const APP_META = Object.freeze({
   name: 'Acelynn Pro™',
-  version: '1.2.0',
+  version: '1.3.0',
   schemaVersion: 1,
-  featureSchemaVersion: 2,
+  featureSchemaVersion: 3,
   spectralDefinition: 'log32-slope-v1',
+  meteringDefinition: 'bs1770-ebu3341-v1',
   copyrightYear: 2026,
   studio: 'Cactus🌵Byte Studios™'
 });
 
 export const RESERVED_VERSION_FIELDS = Object.freeze({
   roomSignatureId: null,
-  roomConfidence: null
+  roomConfidence: null,
+  professionalMetering: null
 });
