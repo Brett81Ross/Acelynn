dd648189763d1f2eea6b2365d4e11a1cd975b663
Brett  Ross <rossbrett05@gmail.com>
2026-09-02 22:18:57 -0500

    test: use standards-compatible Blob in Acelynn QA

--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,11 @@
 import 'fake-indexeddb/auto';
 import { webcrypto } from 'node:crypto';
+import { Blob as NodeBlob } from 'node:buffer';
 
 if (!globalThis.crypto?.subtle) {
   Object.defineProperty(globalThis, 'crypto', { value: webcrypto, configurable: true });
+}
+
+if (!globalThis.Blob?.prototype?.arrayBuffer) {
+  Object.defineProperty(globalThis, 'Blob', { value: NodeBlob, configurable: true });
 }
