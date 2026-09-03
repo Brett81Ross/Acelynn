927fd78f90ad317ec83cedb8cec7ad6de0dbab0c
Brett  Ross <rossbrett05@gmail.com>
2026-09-02 22:09:06 -0500

    feat: stage Acelynn v1.2 metadata contract

---
+++ js/meta.js
@@ -0,0 +1,14 @@
+export const APP_META = Object.freeze({
+  name: 'Acelynn Pro™',
+  version: '1.2.0',
+  schemaVersion: 1,
+  featureSchemaVersion: 2,
+  spectralDefinition: 'log32-slope-v1',
+  copyrightYear: 2026,
+  studio: 'Cactus🌵Byte Studios™'
+});
+
+export const RESERVED_VERSION_FIELDS = Object.freeze({
+  roomSignatureId: null,
+  roomConfidence: null
+});
